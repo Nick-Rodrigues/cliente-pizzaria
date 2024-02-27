1226777a3e1125f671ca76251ebb093665238fd2
Nick-Rodrigues <ncrodrigueslucas@gmail.com>
2024-02-27 09:26:22 -0300

    First

--- cliente-pizzaria-master/src/main/java/br/com/fiap/clientepizzaria/ClientePizzariaApplication.java
+++ cliente-pizzaria-master/src/main/java/br/com/fiap/clientepizzaria/ClientePizzariaApplication.java
@@ -3,11 +3,20 @@ package br.com.fiap.clientepizzaria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
 public class ClientePizzariaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePizzariaApplication.class, args);
 	}
+
+
+	@GetMapping(value = "/")
+	public String index(){
+	return "Bom dia 2TDSPG";
+
+	}
+
 
 }
